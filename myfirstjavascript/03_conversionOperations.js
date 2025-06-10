45fa17ea322c537f76ba9b05f2ae28fc5e1c41d5
Prem Karhale <premkarhale383@gmail.com>
2025-06-11 00:42:24 +0530

    Conversion operations from one datatypes to another

---
+++ myfirstjavascript/03_conversionOperations.js
@@ -0,0 +1,14 @@
+let score = null
+
+let valInNumber = Number(score)
+
+console.log(typeof valInNumber);
+
+console.log(valInNumber);
+
+
+// number datatype converts all typeof into the number , whereas there consol.log is NAN sometimes.
+
+let loggedIN = 1;
+let Booleanconversion = Boolean(loggedIN);
+console.log(Booleanconversion);
