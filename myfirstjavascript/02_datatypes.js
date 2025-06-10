e87edf7a9c032ccdee855152ecf28439ac67d825
Prem Karhale <premkarhale383@gmail.com>
2025-06-11 00:09:23 +0530

    datatypes

---
+++ myfirstjavascript/02_datatypes.js
@@ -0,0 +1,16 @@
+let state;
+//number = 2 to the power 45
+// bigint
+//string =>""
+//boolean = true/false 
+//null = >standalone value 
+//undefined => 
+//symbol => unique 
+//object 
+
+console.log(typeof undefined);//undefined
+console.log(typeof null );//object 
+
+console.log( typeof String); //function 
+console.log( typeof Boolean);//function 
+console.log( typeof Number);//function 
