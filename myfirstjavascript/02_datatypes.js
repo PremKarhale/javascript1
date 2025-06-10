c6e54e0683b084751e11f7746ebece3be03b67c2
Prem Karhale <premkarhale383@gmail.com>
2025-06-11 00:41:04 +0530

    Datatypes

--- myfirstjavascript/02_datatypes.js
+++ myfirstjavascript/02_datatypes.js
@@ -7,10 +7,12 @@ let state;
 //undefined => 
 //symbol => unique 
 //object 
+let Boolean = 45.3; // use case of typeof  ... it is used to know which value is stored inside the variable
+
 
 console.log(typeof undefined);//undefined
 console.log(typeof null );//object 
 
 console.log( typeof String); //function 
-console.log( typeof Boolean);//function 
+console.log( typeof Boolean);//number 
 console.log( typeof Number);//function 
