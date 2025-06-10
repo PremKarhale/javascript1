e1bd841011e3816a3ae96d70b81dd66f57f9c3a6
Prem Karhale <premkarhale383@gmail.com>
2025-06-11 00:09:08 +0530

    variables

---
+++ myfirstjavascript/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 1234;
+var accountholdername = 'prem'
+let accountpassoward = 4536;
+accountcity = 'maharashtra';
+
+// accountId =453;
+console.log(accountId);
+accountholdername ='harsh';
+accountpassoward = 2156;
+accountcity = 'pune';
+console.table([accountId , accountholdername, accountpassoward , accountcity]);
+/*
+const variable cannot be changed in the javascript , whereas other all 
+the function can be changable 
+and we only use let variables not "var "  !!! REMEMBER
+
+
+*/
