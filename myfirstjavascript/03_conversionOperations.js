1f5f1f3ef73daa12c9382f3e92627c203894447a
Prem Karhale <premkarhale383@gmail.com>
2025-06-11 22:08:35 +0530

    conversions_operation

--- myfirstjavascript/03_conversionOperations.js
+++ myfirstjavascript/03_conversionOperations.js
@@ -12,3 +12,23 @@ console.log(valInNumber);
 let loggedIN = 1;
 let Booleanconversion = Boolean(loggedIN);
 console.log(Booleanconversion);
+
+
+//********************* operations **************************** */
+let str1 ='hello'
+let str2 = ' i am prem'
+str3 = str1 + str2
+console.log(str3);
+console.log('1' + 2 + 3); //if string is first than everybody is taken as string ans is 123 
+console.log(1 + 2 + '3');// if string is last than addition of first no and than string ans : 33
+
+
+let x=3
+const y = x++
+console.table([x,y])// 4 , 3 
+
+let x1=3
+const y1 = ++x
+console.table([x1,y1])//4,4
+
+ 
