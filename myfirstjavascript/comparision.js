985c40007f8ad188faa3a06c0e92d011ca6c13ba
Prem Karhale <premkarhale383@gmail.com>
2025-06-13 12:15:04 +0530

    myjavascript

--- myfirstjavascript/comparision.js
+++ myfirstjavascript/comparision.js
@@ -1,3 +1,6 @@
-console.log(null==0)
-console.log(null>=0)
-console.log(null>0)
+console.log(null==0)//false 
+console.log(null>=0) //true 
+console.log(null>0)//false
+
+// aesa kyu , because jo comaprerision operator he or jo equallity operator hote he they work differently 
+
