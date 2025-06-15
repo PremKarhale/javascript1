ed21976b80706438776b0ae6534459a83fa1e740
Prem Karhale <premkarhale383@gmail.com>
2025-06-15 23:46:15 +0530

    variables

--- myfirstjavascript/01_variables.js
+++ myfirstjavascript/01_variables.js
@@ -2,13 +2,13 @@ const accountId = 1234;
 var accountholdername = 'prem'
 let accountpassoward = 4536;
 accountcity = 'maharashtra';
-
+  
 // accountId =453;
 console.log(accountId);
-accountholdername ='harsh';
+accountholdername = 'harsh';
 accountpassoward = 2156;
 accountcity = 'pune';
-console.table([accountId , accountholdername, accountpassoward , accountcity]);
+console.table([accountId, accountholdername, accountpassoward, accountcity]);
 /*
 const variable cannot be changed in the javascript , whereas other all 
 the function can be changable 
@@ -16,3 +16,18 @@ and we only use let variables not "var "  !!! REMEMBER
 
 
 */
+
+function showMessage() {
+    if (true) {
+        var x = 10;
+        let y = 20;
+        const z = 30;
+        console.log(y);       //clearly shown that var are function scoped  and let and const are block scoped 
+        console.log(z);
+    }
+    console.log(x);
+
+
+}
+
+showMessage();
