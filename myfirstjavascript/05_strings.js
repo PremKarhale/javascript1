e8e82ff185a0f31f53c6762c8cb0b65e8d2f3bda
Prem Karhale <premkarhale383@gmail.com>
2025-06-15 23:46:59 +0530

    strings

---
+++ myfirstjavascript/05_strings.js
@@ -0,0 +1,21 @@
+const name = 'premKarhale'
+const age = 20
+let newname = '   sakshi karhale   '
+let emailid = 'prem@123gmail.com%23yahho.com'
+
+// console.log(name + age + " value")
+
+// console.log(`hey my name is ${name} and  my age is ${age}`); //string interpolation 
+
+
+console.log(name.charAt(4));
+console.log(name.indexOf('K')); // !! REMEMBER javascript is a case sensetive language so k != K \
+console.log(name.slice(0,4));
+console.log(newname.trim());
+
+console.log(emailid.replace('%23','-'))
+console.log(emailid.includes('premx'))//false
+
+
+
+
