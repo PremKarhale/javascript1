909276294b773d4e01342f788c40cde4b4654353
Prem Karhale <premkarhale383@gmail.com>
2025-06-17 13:52:33 +0530

    Array

--- myfirstjavascript/array.js
+++ myfirstjavascript/array.js
@@ -32,3 +32,61 @@ console.log(array); // [ 1, 6 ]                        // Splice manipulates the
 
 
 
+//********************************* CONCAT & SPREAD OPERATOR ************************************************ */
+
+const heros = ['ironman', 'spiderman','wanda','hulk'];
+const dc_heroes =['superman','batman','flash'];
+
+
+heros.push(dc_heroes);
+console.log(heros)
+console.log(heros[4][1]) //batman
+
+const together = heros.concat(dc_heroes)
+console.log(together)  
+
+/*
+[
+  'ironman',  
+  'spiderman',
+  'wanda',    
+  'hulk',     
+  'superman', 
+  'batman',   
+  'flash'     
+]
+*/
+const newheroes = [...heros,...dc_heroes];
+console.log(newheroes)    //spread operator
+
+
+const anotherarray = [1,2,3,4,[5,3,4],6,7,2,[1,6,7]];
+const anothernewarray = anotherarray.flat(Infinity);             //flat operator 
+console.log(anothernewarray);   
+
+console.log(Array.isArray('prem'));  //false
+console.log(Array.from('prem'));  //[ 'p', 'r', 'e', 'm' ]
+console.log(Array.from({name:'prem'}))  //
+console.log(Array.from({name:'prem'}))  // i got a empty array  intresting case  !! we have to specify 
+
+
+
+const score1 =45
+const score2 = 56;
+const score3 = 26 ;
+
+console.log(Array.of(score1,score2,score3)); //[ 45, 56, 26 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
