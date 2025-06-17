8255001cfb93285e246978adcfcb48be5fc99c04
Prem Karhale <premkarhale383@gmail.com>
2025-06-17 11:24:38 +0530

    string

--- myfirstjavascript/05_strings.js
+++ myfirstjavascript/05_strings.js
@@ -10,11 +10,15 @@ let emailid = 'prem@123gmail.com%23yahho.com'
 
 console.log(name.charAt(4));
 console.log(name.indexOf('K')); // !! REMEMBER javascript is a case sensetive language so k != K \
-console.log(name.slice(0,4));
-console.log(name.slice(1,4)); 
+
+console.log(name.slice(0,4)); //prem
+
+console.log(name.slice(1,4)); //rem
+
 console.log(newname.trim()); //sakshi karhale   !!REMOVES WIDE SPACES
 
 console.log(emailid.replace('%23','-'))
+
 console.log(emailid.includes('premx'))//false   
 
 
