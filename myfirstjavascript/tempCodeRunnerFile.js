42c9cf2ee95ce09223c47887a6dec9627ba710a2
Prem Karhale <premkarhale383@gmail.com>
2025-06-18 00:29:07 +0530

    Object

--- myfirstjavascript/tempCodeRunnerFile.js
+++ myfirstjavascript/tempCodeRunnerFile.js
@@ -1,7 +1,36 @@
 
-// heros.push(dc_heroes);
-// console.log(heros)
-// console.log(heros[4][1]) //batman
 
-// const together = heros.concat(dc_heroes)
-// console.log(together)  
+// const mysym = Symbol('key1')
+
+// const user ={
+//     name:'prem',
+//     age:30,
+//     emailid : 'prem123@gmail.com',
+//     isloggedIN : false,
+//     [mysym] : 'mykey1',
+//     lastLoginDays  : ['monday','friday ']
+
+// }
+
+// console.log(user.name)
+// console.log(user['name'])// Here name is a string so can be accessed in this way 
+// console.log(user['emailid']) //prem123@gmail.com
+// console.log(user[mysym])
+// console.log(typeof [mysym]) // object 
+
+
+
+// console.log(user.emailid = 'prem345@gmail.com');
+
+// // Object.freeze(user); // now i freeze the object and we cannot change its value now 
+// console.log(user)
+
+// user.greeting = function(){
+//     return ' hello js user '
+// }
+// console.log(user.greeting())
+
+// user.greeting2 = function(){
+//     return `hello  new js user ${this.name} of age ${this.age}`  // this statement 
+// }
+// console.log(user.greeting2());
