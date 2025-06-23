3d5564e1417bcbc0859727c2b1fab3411eee535a
Prem Karhale <premkarhale383@gmail.com>
2025-06-23 15:56:05 +0530

    IIEF

---
+++ IIFE.js
@@ -0,0 +1,26 @@
+// Immediately invoked function Expression (IIFE)
+
+
+//Named IIFE
+(function chai (){         
+console.log(`DB CONECTED `)
+})();   //IIFE IS USED TO INVOKED THE FUNCTION IMMEDIATLY AFTER THE EXECUTION  && ALSOO TO ELIMINATE THE PROBLEM CAUSED BY THE GOLBAL VARIABLE 
+
+         // ; is must  to know that where actually the function is ended 
+
+( (name)=>{
+  console.log(`chai and ${name}`)
+})('code');    //IIFE FOR THE ARROW FUNCTION 
+
+
+
+console.log(
+    
+((parameter)=>{
+
+    return parameter*3;     //calling the function immediately after the execution 
+})(4)
+);
+
+
+
