1af754024b24241d203513b10d40ac58b02568c9
Prem Karhale <premkarhale383@gmail.com>
2025-07-02 10:11:10 +0530

    Temp

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,28 +1,2 @@
 
-
-// if (true){
-//     let a = 23;
-//     const b = 4;
-//      var c = 345;
-//      console.log(a) 
-// console.log(b) 
-// }
-// console.log(a) //error     let is block scoped 
-// console.log(b) //error    const is block scoped 
-// console.log(c) //345       // variable is global scoped 
-
-
-// function one (){
-//     const username = 'prem'
-//     const age = 20;
-
-//     function two (){
-//         const website = 'youtube';
-//         console.log(username);     // child can acces the variables in the parent function 
-//     }
-//     // console.log(website) //error ,scope is blocked 
-//     two(); //print  --> prem
-//     console.log(age);
-// }
-// one()
-
+    for (let j = 1; j <=10; j++) {
