fcce967886dead0b73b4a3af9e920b31ad20c666
Prem Karhale <premkarhale383@gmail.com>
2025-07-13 12:38:03 +0530

    DOM1

--- myfirstjavascript/Projects/DOM Project1/script.js
+++ myfirstjavascript/Projects/DOM Project1/script.js
@@ -1,11 +1,16 @@
 
-
+//  const date = new Date();
+//     console.log(date.toDateString())
 const clock = document.querySelector('.clock')
 
 setInterval(function () {
-
+                                       // set interval is used to "set the time of recurrence" of the event occured 
     const date = new Date();
-const actualDate = (date.toLocaleTimeString())
-    clock.innerHTML = `${actualDate}`
+
+    const actualDate = (date.toLocaleTimeString())
+    const newdate = date.toDateString()
+    clock.innerHTML = `${actualDate} <div class ="div">${date.toDateString()}</div>`
+    
 
 }, 1000)
+
